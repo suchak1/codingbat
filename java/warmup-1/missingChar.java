3326c3e65d1df8f09b75e85c6e46bf7859fca440
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2018-12-07 03:33:50 -0600

    Create missingChar.java

---
+++ java/warmup-1/missingChar.java
@@ -0,0 +1,15 @@
+/*
+
+Given a non-empty string and an int n, return a new string 
+where the char at index n has been removed. The value of n 
+will be a valid index of a char in the original string 
+(i.e. n will be in the range 0..str.length()-1 inclusive).
+
+*/
+
+public String missingChar(String str, int n) {
+  String s = str.substring(0, n);
+  String g = str.substring(n + 1);
+    
+  return s + g;
+}
