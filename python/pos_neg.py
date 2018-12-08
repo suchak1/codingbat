657ecc43f53306139e37270795e74c9c21b62f3b
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2018-12-07 20:40:04 -0600

    Update pos_neg.py

--- python/pos_neg.py
+++ python/pos_neg.py
@@ -1,10 +1,10 @@
-/*
+"""
 
 Given 2 int values, return True if one is negative and one is positive. 
 Except if the parameter "negative" is True, 
 then return True only if both are negative.
 
-*/
+"""
 
 
 def pos_neg(a, b, negative):
