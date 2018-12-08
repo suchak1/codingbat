57cc5aa1c2f79c5efeae291bf634cd59b47bfc76
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2018-12-07 19:43:37 -0600

    Create intMax.java

---
+++ java/warmup-1/intMax.java
@@ -0,0 +1,17 @@
+/*
+
+Given three int values, a b c, return the largest.
+
+*/
+
+
+public int intMax(int a, int b, int c) {
+  
+  if(a >= b && a >= c)
+    return a;
+  else if(b >= c)
+    return b;
+  else
+    return c;
+  
+}
