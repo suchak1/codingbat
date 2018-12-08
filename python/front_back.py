92421504932144c9b7400f49cbbd11cfd676c6fd
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2018-12-07 20:42:07 -0600

    Create front_back.py

---
+++ python/front_back.py
@@ -0,0 +1,9 @@
+"""
+
+Given a string, return a new string where the first and last chars have been exchanged.
+
+"""
+
+
+def front_back(str):
+  return str if len(str) == 1 else str[-1:] + str[1:-1] + str[:1]
